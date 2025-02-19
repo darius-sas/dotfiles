f028070980fbbaf2a936aac9bc992b95ff86e620
Darius Sas <darius.sas@outlook.com>
2025-02-19 08:02:37 +0100

    feat: rose pine

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fzf --fish | source
     jump shell fish | source
+    pyenv init - fish | source
 end
---
+++ fish/.config/fish/functions/miner.fish
@@ -0,0 +1,3 @@
+function miner
+/home/fenn/projects/impact/impact-repo-miner/target/release/cli $argv
+end
