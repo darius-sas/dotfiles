dea10c061fd2330a055d02b7c39adfb1a8562cc9
Darius Sas <darius.sas@outlook.com>
2025-03-11 15:22:31 +0100

    fix: non-current window background same as current

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -800,6 +800,7 @@ require('lazy').setup({
     config = function()
       vim.cmd("colorscheme rose-pine")
       vim.cmd("highlight Normal guibg=#181818")
+      vim.cmd("highlight NormalNC guibg=#181818")
       vim.cmd("highlight NormalFloat guibg=#181818")
     end
   },
