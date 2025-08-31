0480469deb179bbc840dd05c7173f26d4600bf91
Darius Sas <darius.sas@outlook.com>
2025-08-31 10:46:34 +0200

    feat: added default formatters

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -658,8 +658,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { "black" },
+        rust = { "rustfmt" },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
