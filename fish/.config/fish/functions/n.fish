793ca8b45fcf70a5b21a29ae55e53560a47532dc
Darius Sas <darius.sas@outlook.com>
2025-09-01 12:30:35 +0200

    feat: check non-existing directory before jumping

--- fish/.config/fish/functions/n.fish
+++ fish/.config/fish/functions/n.fish
@@ -1,5 +1,10 @@
 function n
     set dst (z -e $argv)
+    set success $status
+    if test "$success" -ne 0
+        echo "Directory not in z index. Doing nothing."
+        return 1
+    end
     cd $dst
     nvim .
 end
