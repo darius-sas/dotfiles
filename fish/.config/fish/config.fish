3e8b5e598f80582373f5105da9cb6fba1fea2804
Darius Sas <darius.sas@outlook.com>
2025-11-24 14:38:06 +0100

    feat: fish venv and inits

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,5 +3,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fzf --fish | source
     # jump shell fish | source
-    pyenv init - fish | source
+    # pyenv takes a bit to run
+    # pyenv init - fish | source
+    # Scaleway CLI autocomplete initialization.
+    # eval (scw autocomplete script shell=fish)
 end
+
---
+++ fish/.config/fish/functions/r2g.fish
@@ -0,0 +1,3 @@
+function r2g
+    /home/fenn/projects/impact-2/impact-repo2graph/target/release/r2g $argv
+end
